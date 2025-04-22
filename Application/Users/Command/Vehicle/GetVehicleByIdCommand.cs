a0d9248b374e04b98421cc32d9cb97e55ede3c4b
mateuszigbt <mniebora@gmail.com>
2025-04-22 16:15:32 +0200

    Downloading offer by Id.

---
+++ Application/Users/Command/Vehicle/GetVehicleByIdCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Users.DTOs.Vehicle;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.Command.Vehicle
+{
+    public class GetVehicleByIdCommand : IRequest<GetAllVehicleDto>
+    {
+        public int VehicleId { get; set; }
+    }
+}
